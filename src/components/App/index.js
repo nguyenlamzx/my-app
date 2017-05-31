e52977c3e57dca696d2b1ffe1b349bc920298446
Lam Nguyen <nguyenlamzx@outlook.com>
2017-05-31 09:53:12 +0700

    [update] index.js

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
 
-import Header from '../Header'
+import Header from 'components/Header'
 
 import logo from 'assets/logo.svg'
 

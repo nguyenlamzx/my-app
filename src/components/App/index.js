b594c548eef9fa4d67e404377d071a8ea93398e4
Lam Nguyen <nguyenlamzx@outlook.com>
2017-06-16 08:23:45 +0700

    update

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -9,84 +9,29 @@ import Todo from 'components/Todo';
 import Home from 'containers/Home';
 import About from 'containers/About';
 import Topic from 'containers/Topic';
+import Topics from 'containers/Topics';
 
 import logo from '../../assets/logo.svg'
-
-const Topics = ({ match }) => (
-  <div>
-    <h2>Topics</h2>
-    <ul>
-      <li>
-        <Link to={`${match.url}/rendering`}>
-          Rendering with React
-        </Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/components`}>
-          Components
-        </Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/props-v-state`}>
-          Props v. State
-        </Link>
-      </li>
-    </ul>
-
-    <Route path={`${match.url}/:topicId`} component={Topic}/>
-    <Route exact path={match.url} render={() => (
-      <h3>Please select a topic.</h3>
-    )}/>
-  </div>
-)
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <div>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">About</Link></li>
-            <li><Link to="/topics">Topics</Link></li>
-          </ul>
-
-          <hr/>
-
-          {/*<Route exact path="/" component={Home}/>*/}
-          <Route path="/about" component={About}/>
-          <Route path="/topics" component={Topics}/>
-          <Wrapper>
-            <Header {...{logo}} />
-            <ul>
-              <li>
-                <Link to="/">
-                  Home
-                </Link>
-              </li>
-              <li>
-                <Link to={`/todo`}>
-                  Todo
-                </Link>
-              </li>
-              {/*<li>
-                <Link to={`/about`}>
-                  About
-                </Link>
-              </li>*/}
-            </ul>
-
-            <div>
-              <Route exact path="/" render={() => (
-                <Intro>
-                  To get started, edit <code>src/App.js</code> and save to reload.
-                </Intro>
-              )}/>
-              <Route path="/todo" component={Todo}/>
-              {/*<Route path="/news" component={NewsFeed}/>*/}
-            </div>
-          </Wrapper>
-        </div>
+        <Wrapper>
+          <Header {...{logo}} />
+          <div>
+            <Route exact path="/" component={Home}/>
+            <Route path="/about" component={About}/>
+            <Route path="/topics" component={Topics}/>
+            <Route exact path="/" render={() => (
+              <Intro>
+                To get started, edit <code>src/App.js</code> and save to reload.
+              </Intro>
+            )}/>
+            <Route path="/todo" component={Todo}/>
+            {/*<Route path="/news" component={NewsFeed}/>*/}
+          </div>
+        </Wrapper>
       </Router>
     )
   }
